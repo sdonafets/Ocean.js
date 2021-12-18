d635022409d7d50eb4857f0fa8388df9b87145bc
sdonafets <stefano.desantis4@libero.it>
2021-12-18 19:53:59 +0100

    Base project setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pm.js
+
+pm.js is a fullstack app where ...
+
+The app is built using the following technologies:
+- Next.js
+- Apollo
+- Prisma
+- PostgreSQL
+- Auth0
+- TypeScript
+- TailwindCSS
---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/app";
+import { Layout } from "@/components/Layout";
+import "@/styles/tailwind.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Layout><Component {...pageProps} /></Layout>
+}
+
+export default MyApp;
