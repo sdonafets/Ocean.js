cd74bb38de1a3228488cee7d5a6dd21fd1cadb1f
sdonafets <stefano.desantis4@libero.it>
2021-12-21 17:46:17 +0100

    [OJS-FIX] Navbar

--- components/Layout/Navbar/index.tsx
+++ components/Layout/Navbar/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Image from "next/image";
 import styles from "./Navbar.module.scss";
-import logo from "../../../public/assets/wave.png";
 import {useRouter} from "next/router";
 import {Button} from "antd";
 import {MessageOutlined} from "@ant-design/icons";
@@ -14,11 +12,11 @@ const Navbar = () => {
             <Button
                 className={styles.menu}
                 type="ghost"
-                icon={<Image src={logo} className={styles.logo} alt="logo"/>}
                 onClick={() => router.push("/")}
-            />
+            >Ocean.js</Button>
             <div>
-                <Link passHref href={"mailto:stefano.desantis4@libero.it"}><MessageOutlined/></Link>
+                <Link passHref href={"mailto:stefano.desantis4@libero.it"}><MessageOutlined
+                    style={{fontSize: "20px", paddingTop: "4px", paddingBottom:"4px"}}/></Link>
             </div>
         </nav>
     );
